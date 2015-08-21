1420623e48d9a7d0c4fb67419822ebaf78f8f44f
Miguel Mayori <miguel.mayori@coderoad.com>
2015-08-20 20:23:56 -0400

    Languages

---
+++ local/app/lang/es/core.php
@@ -0,0 +1,41 @@
+<?php
+
+
+return [
+	/*
+	|--------------------------------------------------------------------------
+	| Text Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the Static text shown on website.
+	| You are free to change them to anything
+	| you want to customize your views to better match your application.
+	|
+	*/
+
+
+	'absent'           =>  'Absent',
+	'present'          =>  'Present',
+	'attendance'       =>  'Attendance',
+
+	/*
+     * Button text
+     */
+	'btnAddEmployee'    => 'Add New Employee',
+	'btnAddDepartment'  => 'Add New Department',
+	'btnAddAward'       => 'Add New Award',
+	'btnAddExpense'     => 'Add New Expense',
+	'btnAddHoliday'     => 'Add New Holiday',
+	'btnAddLeaveType'   => 'Add New LeaveType',
+	'btnAddNotice'      => 'Add New Notice',
+
+	'btnViewEdit'       => 'Ver/Editar',
+	'btnDelete'         => 'Eliminar',
+	'btnSubmit'         => 'Aceptar',
+	'btnSave'           => 'Guardar',
+	'btnUpdate'         => 'Actualizar',
+	'btnUpdating'       => 'Actualizando',
+
+	'btnYes'           =>  'Si',
+	'btnNo'            =>  'No'
+];
