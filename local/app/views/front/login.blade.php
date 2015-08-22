694db1618437715658f25cedcd6fc7c256722e28
ChiGY <gdavid.ptorrez@gmail.com>
2015-08-22 19:15:22 -0400

    Login Beneficiario

--- local/app/views/front/login.blade.php
+++ local/app/views/front/login.blade.php
@@ -10,8 +10,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-
-
     <!-- CSS Global Compulsory -->
     {{HTML::style('front_assets/plugins/bootstrap/css/bootstrap.min.css')}}
     {{HTML::style('front_assets/css/style.css')}}
