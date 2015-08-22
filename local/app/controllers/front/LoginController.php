285b2b6aa04b52e291220e5f8134b1b578716b99
ChiGY <gdavid.ptorrez@gmail.com>
2015-08-22 18:13:38 -0400

    Login Beneficiario

--- local/app/controllers/front/LoginController.php
+++ local/app/controllers/front/LoginController.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+
 class LoginController extends \BaseController {
 
 
@@ -31,29 +33,22 @@ class LoginController extends \BaseController {
         {
             $output =   [];
             $input  = Input::all();
-
             $data	=	[
                 'email'	    =>	$input['email'],
                 'password'	=>	$input['password'],
 	            'status'    =>  'active'
             ];
-
             //Rules to validate the incoming username and password
             $rules  =[
                 'email'	    => 'required|email',
                 'password'	=>	'required'
             ];
-
             $validator	= Validator::make($input,$rules);
-
-
             //if validator fails then move to this block
             if($validator->fails())
             {
                 $output['status'] = 'error';
-//                $output['msg']    =  $validator->getMessageBag()->toArray();
-                $output['msg']    =  'ERROR EN LA VALIDACION';
-
+                $output['msg']    =  $validator->getMessageBag()->toArray();
             }
             // Check if employee exists in database with the credentials of not
 //            if (Auth::beneficiarios()->attempt($data))
@@ -71,9 +66,9 @@ class LoginController extends \BaseController {
                 return Response::json($output, 200);
             }
 	        // For Blocked Users
-	        $data['status']         =   'inactive';
-
-            if(Auth::beneficiarios()->validate($data))
+//	        $data['status']         =   'inactive';
+//            if(Auth::beneficiarios()->validate($data))
+            if(Auth::attempt($data))
             {
 	            $output['status']	=	'error';
 	            $output['msg']		=	['error'=>Lang::get('messages.loginBlocked')];
