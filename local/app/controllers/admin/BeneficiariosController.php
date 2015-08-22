a53b164923d441ee96b8a8cc9d9fe9f7b3f9043a
Miguel Mayori <miguel.mayori@coderoad.com>
2015-08-22 19:00:49 -0400

    Destinos y Obetivos

--- local/app/controllers/admin/BeneficiariosController.php
+++ local/app/controllers/admin/BeneficiariosController.php
@@ -381,7 +381,7 @@ class BeneficiariosController extends \AdminBaseController {
 			                        'beneficiarios.fechaing','beneficiarios.direccion','beneficiarios.direccionperm','beneficiarios.status',
 			                        'beneficiarios.fecha_desvinculacion',
 			                        'zonificacion_beneficiario.departamento','zonificacion_beneficiario.zona','zonificacion_beneficiario.otros'
-		                        )->orderBy('beneficiarios.id','asc')
+		                        )->orderBy('beneficiarios.apellidos','asc')
 		                        ->get()->toArray();
 
 		$data = $ben;
