6e9124970d38fb59f920eb0afd53c9b7fe34025c
Miguel Mayori <miguel.mayori@coderoad.com>
2015-08-22 10:44:22 -0400

    Destinos y Obetivos

--- local/app/views/admin/destinos/index.blade.php
+++ local/app/views/admin/destinos/index.blade.php
@@ -280,7 +280,7 @@
              if (countIds>0) {
                  $j= countIds;
              }
-             console.log($j);
+             console.log(countIds);
             $('#plus_edit_Button').click(function(){
               $j = $j+1;
               $(' <div class="form-group" id="edit_field"> <div class="col-md-12"><input class="form-control form-control-inline input-medium"  name="objetivo['+$j+']" type="text"  placeholder="Objetivo #'+($j+1)+'"/></div></div>').insertBefore($insertBefore_edit);
