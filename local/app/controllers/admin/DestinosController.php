431c65bb5a324d2a883de67ce90b85558aed92b6
Miguel Mayori <miguel.mayori@coderoad.com>
2015-08-22 09:45:26 -0400

    Destinos y Obetivos

--- local/app/controllers/admin/DestinosController.php
+++ local/app/controllers/admin/DestinosController.php
@@ -65,7 +65,7 @@ class DestinosController extends \AdminBaseController {
 	 */
 	public function edit($id)
 	{
-		echo "muere";
+		echo $id;
 		die();
 		$this->data['objetivos'] = Objetivo::find($id);
 		return View::make('admin.destinos.edit', $this->data);
