c524344fc44dafec74be957d0936556da444843d
Miguel Mayori <miguel.mayori@coderoad.com>
2015-08-22 10:02:12 -0400

    Destinos y Obetivos

--- local/app/views/admin/destinos/edit.blade.php
+++ local/app/views/admin/destinos/edit.blade.php
@@ -13,8 +13,8 @@
 <div class="form-group">
 
  <div class="col-md-6">
-    <input class="form-control form-control-inline input-medium" name="designation[{{$i}}]" id="designation" type="text" value="{{$objetivo['destino']}}" placeholder="Destino #1"/>
-    <input type="hidden" name="objetivoID[{{$i}}]" id="objetivo" type="text" value="{{$objetivo['id']}}" placeholder="Objetivo #1"/>
+    <input class="form-control form-control-inline input-medium" name="objetivo[{{$i}}]" id="objetivo" type="text" value="{{$objetivo['objetivo']}}" placeholder="Destino #1"/>
+    <input type="hidden" name="objetivoID[{{$i}}]" id="" type="text" value="{{$objetivo['id']}}" placeholder="Objetivo #1"/>
  </div>
 {{'';$i++;}}
 </div>
