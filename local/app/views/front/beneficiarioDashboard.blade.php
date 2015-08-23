464b1bd07f9438ceaab943d3f680a5a612338be2
Miguel Mayori <miguel.mayori@coderoad.com>
2015-08-22 20:42:35 -0400

    Destinos y Obetivos

--- local/app/views/front/beneficiarioDashboard.blade.php
+++ local/app/views/front/beneficiarioDashboard.blade.php
@@ -208,19 +208,19 @@
                         <div class="panel-heading overflow-h">
                             <h2 class="panel-title heading-sm pull-left"><i class="fa fa-trophy"></i> Donaciones Recibidas</h2>
                         </div>
-                        <div id="scrollbar3" class="panel-body contentHolder">
+                        {{--<div id="scrollbar3" class="panel-body contentHolder">--}}
 
-                            @foreach($awards as $award)
-                                <div class="alert-blocks">
-                                    <div class="overflow-h">
-                                        <strong class="color-dark">{{Str::words($award->employeeDetails->fullName,1,'')}} <small class="pull-right"><em>{{ucfirst($award->forMonth)}} {{$award->forYear}}</em></small></strong>
-                                        <small class="award-name">{{$award->awardName}}</small>
-                                    </div>
-                                </div>
-                            @endforeach
+                            {{--@foreach($awards as $award)--}}
+                                {{--<div class="alert-blocks">--}}
+                                    {{--<div class="overflow-h">--}}
+                                        {{--<strong class="color-dark">{{Str::words($award->employeeDetails->fullName,1,'')}} <small class="pull-right"><em>{{ucfirst($award->forMonth)}} {{$award->forYear}}</em></small></strong>--}}
+                                        {{--<small class="award-name">{{$award->awardName}}</small>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--@endforeach--}}
 
 
-                        </div>
+                        {{--</div>--}}
                     </div>
                     <hr>
 
