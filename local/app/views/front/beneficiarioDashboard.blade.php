ce2bb2f38fce85440318cc573fc8f6cbe37299cd
ChiGY <gdavid.ptorrez@gmail.com>
2015-08-24 13:30:31 -0400

    beneficiario zonificacion

--- local/app/views/front/beneficiarioDashboard.blade.php
+++ local/app/views/front/beneficiarioDashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('front.layouts.frontlayout')
 @section('mainarea')
-    {{dd($zonificacion)}}
+    {{dd($zonificacion>departamento)}}
     <div class="col-md-9">
         <!--Profile Body-->
         <div class="profile-body">
