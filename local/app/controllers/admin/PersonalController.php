0d91417844383e586afdcc934be09cc0d0f3d0e3
ChiGY <gdavid.ptorrez@gmail.com>
2015-08-25 19:13:50 -0400

    menu personal

--- local/app/controllers/admin/PersonalController.php
+++ local/app/controllers/admin/PersonalController.php
@@ -26,8 +26,9 @@ class PersonalController extends \AdminBaseController {
 	 */
 	public function create()
 	{
-		$this->data['personalActive']  =   'active';
-		return View::make('admin.personal.create',$this->data);
+        dd("hola a todos");
+//		$this->data['personalActive']  =   'active';
+//		return View::make('admin.personal.create',$this->data);
 	}
 	/**
 	 * Store a newly created employee in storage
