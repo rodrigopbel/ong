eb478ed94dbf300db8f335441b9608bba4224857
Viviana Fernandez <viviana.f8a@gmail.com>
2015-08-26 18:01:47 -0300

    "modificacion act"

--- assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js
+++ assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js
@@ -1424,8 +1424,8 @@
 			days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
 			daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
 			daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-			months: ["Enero", "Febrero", "Marzo", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-			monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+			months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+			monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dic"],
 			today: "Today",
 			clear: "Clear"
 		}
