e6962a80ca4329e73dba68201c7dca92b2d1987e
ChiGY <gdavid.ptorrez@gmail.com>
2015-08-28 10:24:38 -0400

    modulo de personas

--- local/app/controllers/admin/PersonalController.php
+++ local/app/controllers/admin/PersonalController.php
@@ -38,12 +38,12 @@ class PersonalController extends \AdminBaseController {
     {
 //        dd(Input::all());
 //        dd("cerca a la calidacion");
-        $validator = Validator::make($input = Input::all(), Personal::rules('create'));
-        if ($validator->fails())
-        {
-            return Redirect::back()->withErrors($validator)->withInput();
-        }
-        dd($validator);
+//        $validator = Validator::make($input = Input::all(), Personal::rules('create'));
+//        if ($validator->fails())
+//        {
+//            return Redirect::back()->withErrors($validator)->withInput();
+//        }
+        dd("la validacion estaba mal");
     }
     /**
      * Show the form for editing the specified employee
