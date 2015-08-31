cf2662f3fe9f331a87e646ab853ffd09ca75028d
Viviana Fernandez <viviana.f8a@gmail.com>
2015-08-31 14:52:36 -0300

    "modificacion ayuda"

--- local/app/views/admin/include/sidebar.blade.php
+++ local/app/views/admin/include/sidebar.blade.php
@@ -115,12 +115,12 @@
                 <li class="{{ $awardsOpen or ''}}">
                     <a href="javascript:;">
                         <i class="fa fa-trophy"></i>
-                        <span class="title">{{Lang::get('menu.ayuda')}}</span>
+                        <span class="title">{{Lang::get('menu.ayudas')}}</span>
                         <span class="arrow "></span>
                     </a>
                     <ul class="sub-menu">
                         <li class="{{ $awardsActive or ''}}">
-                            <a href="{{route('admin.ayuda.index')}}">
+                            <a href="{{route('admin.ayudas.index')}}">
                                 <i class="fa  fa-gift"></i>
                                 {{Lang::get('menu.awardList')}}</a>
                         </li>
