ac1e04250fd41f4a2ccccfce0e48a67668ff23d9
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-01 15:32:43 -0400

    modulo de personas

--- local/app/controllers/admin/ActividadesController.php
+++ local/app/controllers/admin/ActividadesController.php
@@ -30,7 +30,7 @@ class ActividadesController extends \AdminBaseController {
             $act[date('F', strtotime($actividad->date))]['day'][] = date('D', strtotime($actividad->date));
         }
         $this->data['actividadesArray'] = $act;
-        dd( $this->data);
+        dd(  $this->data['actividadesArray']);
         return View::make('admin.actividades.index', $this->data);
     }
 
