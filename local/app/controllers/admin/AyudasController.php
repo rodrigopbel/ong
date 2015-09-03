37fa545472d95b3461eef1432ea2975cb49d567a
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-03 14:58:57 -0300

    "modificacion ayuda"

--- local/app/controllers/admin/AyudasController.php
+++ local/app/controllers/admin/AyudasController.php
@@ -36,7 +36,7 @@ class AyudasController extends \AdminBaseController {
             })
             ->add_column('edit', '
                         <a  class="btn purple"  href="{{ route(\'admin.ayudas.edit\',$id)}}" ><i class="fa fa-edit"></i> View/Edit</a>
-                            &nbsp;<a href="javascript:;" onclick="del(\'{{ $id }}\',\'{{ $tipo_aporte}}\',\'{{ $montoaporte }}\');return false;" class="btn red">
+                            &nbsp;<a href="javascript:;" onclick="del(\'{{ $id }}\',\'{{ $apellidos}}\',\'{{ $montoaporte }}\');return false;" class="btn red">
                         <i class="fa fa-trash"></i> Borrar</a>')
 
             ->remove_column('porelAnio')
@@ -52,7 +52,7 @@ class AyudasController extends \AdminBaseController {
         $this->data['beneficiarios'] = Beneficiario::selectRaw('CONCAT(apellidos, " (EmpID:", beneficiarioID,")") as apellidos, beneficiarioID')
 	                                        ->where('status','=','active')
 	                                        ->lists('apellidos','beneficiarioID');
-    dd($this->data['beneficiarios']);
+
 		return View::make('admin.ayudas.create',$this->data);
 	}
 
