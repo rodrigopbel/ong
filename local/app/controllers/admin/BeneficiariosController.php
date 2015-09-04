ba644c6fd154aed6bc8e2e7b5f140d222a20d546
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-04 10:50:12 -0400

    modulo de personas

--- local/app/controllers/admin/BeneficiariosController.php
+++ local/app/controllers/admin/BeneficiariosController.php
@@ -181,8 +181,8 @@ class BeneficiariosController extends \AdminBaseController {
         $this->data['destinos']      =     Destino::lists('destino','id');
         $this->data['beneficiario']  =   Beneficiario::where('beneficiarioID', '=' ,$id)->get()->first();
         $this->data['objetivo']      =   Objetivo::find($this->data['beneficiario']->objetivo);
-        $this->data['responsable']      =  Personal::where('beneficiarioID','=',$id)->get()->first();
-        dd($this->data['beneficiario'] );
+        $this->data['responsable']      =  Personal::where('beneficiarioID','=',$id)->where('tipoPersonal', '=', 'responsable')->get()->first();
+
         $doc = [];
 //        dd($this->data['responsable']);
         foreach($this->data['beneficiario']->getDocuments as $documents)
@@ -190,8 +190,8 @@ class BeneficiariosController extends \AdminBaseController {
             $doc[$documents->type] =  $documents->fileName ;
         }
         $this->data['documents']  =   $doc;
-        $this->data['zonificacion']     =   Zonificacion::where('beneficiarioID', '=' ,$id)->where('tipoPersonal', '=', 'responsable')->get()->first();
-
+        $this->data['zonificacion']     =   Zonificacion::where('beneficiarioID', '=' ,$id)->get()->first();
+        dd($this->data['beneficiario'] );
         return View::make('admin.beneficiarios.edit', $this->data);
     }
 
