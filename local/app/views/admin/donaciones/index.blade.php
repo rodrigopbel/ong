1a8c571f3c5cc0b5244ba21242b5da755085e895
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-04 16:14:21 -0300

    "modificacion donaciones"

--- local/app/views/admin/donaciones/index.blade.php
+++ local/app/views/admin/donaciones/index.blade.php
@@ -20,6 +20,7 @@
 				<ul class="page-breadcrumb">
 					<li>
 						<i class="fa fa-home"></i>
+                        dd("hola");
 						<a href="{{route('admin.dashboard.index')}}">Inicio</a>
 						<i class="fa fa-angle-right"></i>
 					</li>
