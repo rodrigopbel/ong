974d8fe93ee681380b1db00e68ae1b3f752e0bee
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-07 18:53:46 -0300

    actualizacion donaciones

--- local/app/views/admin/donaciones/index.blade.php
+++ local/app/views/admin/donaciones/index.blade.php
@@ -68,7 +68,8 @@
 								<th> Descripcion</th>
 								<th> Monto </th>
 								<th> Fecha Creacion </th>
-                                {{--<th> Fecha Creacion </th>--}}
+                                <th> Fecha Creacion </th>
+                                <th> Fecha Creacion </th>
 
 
 								<th> Accion </th>
