a83e0a8869cbd1892fe5bbcc69e1765a8bd2a2b6
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-08 18:07:39 -0400

    report saldo

--- local/app/routes.php
+++ local/app/routes.php
@@ -72,6 +72,7 @@ Route::group(array('prefix' => 'admin','before' => 'auth.admin|lock'), function(
     Route::get('ajax_donaciones/',['as'=>'admin.ajax_donaciones','uses'=> 'DonacionesController@ajax_donaciones']);
     Route::resource('donaciones', 'DonacionesController',['as' => 'admin']);
     // Reportes Routing
+    Route::get('ajax_reportes/',['as'=>'admin.ajax_reportes','uses'=> 'ReportesController@ajax_reportes']);
     Route::resource('reportes','ReportesController',['as'=>'admin']);
 //    Route::get('reports/beneficiarios','ReportsController@reportesben',['as'=>'admin']);
 //    Route::get('reports/transacciones','ReportsController@reportestran',['as'=>'admin']);
