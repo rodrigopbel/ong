95e61659401b2828ead355c3e7c198c06fe5b412
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-08 19:43:11 -0400

    report saldo

--- local/app/controllers/admin/ReportesController.php
+++ local/app/controllers/admin/ReportesController.php
@@ -26,7 +26,7 @@ class ReportsController extends \AdminBaseController {
             Ayuda::select('ayudas.id','beneficiarios.beneficiarioID','apellidos','requerimiento','centroSalud','nit','numfactura','gastos','ayudas.created_at')
                 ->join('beneficiarios', 'ayudas.beneficiarioID', '=', 'beneficiarios.beneficiarioID')
                 ->orderBy('ayudas.created_at','desc');
-        dd($result);
+        dd("maldita sea");
 //        return View::make('admin.reportes.index', $this->data);
     }
 
