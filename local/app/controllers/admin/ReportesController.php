b476a4c295bdfb8bf9c3f465180d8c038f2b5b84
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-08 17:56:47 -0400

    report saldo

--- local/app/controllers/admin/ReportesController.php
+++ local/app/controllers/admin/ReportesController.php
@@ -1,36 +1,37 @@
 <?php
 
 class ReportsController extends \AdminBaseController {
+
     public function __construct()
     {
         parent::__construct();
-        $this->data['reportOpen'] ='active open';
-        $this->data['pageTitle'] =  'Reportes';
+        $this->data['reportesOpen'] ='active open';
+        $this->data['pageTitle']  =  'Reportes';
     }
+
+    //    Display a listing of awards
     public function index()
     {
-//        $this->data['ayudas'] = Ayuda::all();
-//        $this->data['beneficiarios'] = Beneficiario::all();
-//        $this->data['ayudasActive'] =   'active';
-//        $this->data['reporte'] = Ayuda::select('ayudas.gastos','donaciones.montodonacion','donaciones.aportanteID','ayudas.requerimiento','ayudas.created_at','beneficiarios.nombres','beneficiarios.apellidos')
-//                                ->join('beneficiarios', 'ayudas.beneficiarioID','=','beneficiarios.beneficiarioID')
-//                                ->join('donaciones', 'ayudas.aportanteID','=','donaciones.aportanteID')
-//                                ->orderBy('ayudas.created_at','desc');
+        $this->data['reportes'] = Reporte::all();
 
+        $this->data['reportesActive'] =   'active';
+
+        return View::make('admin.reportes.index', $this->data);
+    }
+
+
+    //Datatable ajax request
+    public function ajax_reportes()
+    {
 
         $result =
-                Ayuda::select('ayudas.gastos','ayudas.requerimiento','ayudas.created_at','beneficiarios.nombres','beneficiarios.apellidos')
-                    ->join('beneficiarios', 'ayudas.beneficiarioID','=','beneficiarios.beneficiarioID')
-//                    ->join('donaciones', 'ayudas.aportanteID','=','donaciones.aportanteID')
-                    ->orderBy('ayudas.created_at','desc');
+            Ayuda::select('ayudas.id','beneficiarios.beneficiarioID','apellidos','requerimiento','centroSalud','nit','numfactura','gastos','ayudas.created_at')
+                ->join('beneficiarios', 'ayudas.beneficiarioID', '=', 'beneficiarios.beneficiarioID')
+                ->orderBy('ayudas.created_at','desc');
 
+        return Datatables::of($result)
 
-//        $result =
-//            Ayuda::select('ayudas.id','beneficiarios.beneficiarioID','apellidos','requerimiento','centroSalud','nit','numfactura','gastos','ayudas.created_at')
-//                ->join('beneficiarios', 'ayudas.beneficiarioID', '=', 'beneficiarios.beneficiarioID')
-//                ->orderBy('ayudas.created_at','desc');
-        dd($result);
-
+            ->make();
     }
     public function reportesben()
     {
@@ -42,38 +43,5 @@ class ReportsController extends \AdminBaseController {
         $this->data['reporteActive']    =   'active';
         return "hola reportes Transacciones";
     }
-    public function store()
-    {
-
-        $validator = Validator::make($input = Input::all(), Salary::$rules);
-
-        if ($validator->fails())
-        {
-            return Redirect::back()->withErrors($validator)->withInput();
-        }
-
-        Salary::create($input);
-        Session::flash('success',"<strong>{$input['type']}</strong> Created");
-        return Redirect::route('admin.employees.edit',Input::get('employeeID'));
-    }
-    public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /salary/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-        Salary::destroy($id);
-        $output['success']  =   'deleted';
-        $output['msg']      =   'Salary Deleted successfully';
-        return Response::json($output, 200);
-	}
 
 }
---
+++ local/app/views/admin/reportes/index.blade.php
@@ -0,0 +1,210 @@
+@extends('admin.adminlayouts.adminlayout')
+
+@section('head')
+	<!-- BEGIN PAGE LEVEL STYLES -->
+	{{HTML::style("assets/global/plugins/select2/select2.css")}}
+	{{HTML::style("assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css")}}
+	<!-- END PAGE LEVEL STYLES -->
+
+@stop
+
+
+@section('mainarea')
+
+
+			<!-- BEGIN PAGE HEADER-->
+			<h3 class="page-title">
+			{{$pageTitle}}
+			</h3>
+			<div class="page-bar">
+				<ul class="page-breadcrumb">
+					<li>
+						<i class="fa fa-home"></i>
+						<a href="{{route('admin.dashboard.index')}}">Inicio</a>
+						<i class="fa fa-angle-right"></i>
+					</li>
+					<li>
+						<a href="#">Reportes</a>
+						<i class="fa "></i>
+					</li>
+
+				</ul>
+
+			</div>
+			<!-- END PAGE HEADER-->
+			<!-- BEGIN PAGE CONTENT-->
+									{{--<div class="row">--}}
+                        				{{--<div class="col-md-6">--}}
+
+				 {{--<a class="btn green" data-toggle="modal" href="{{URL::to('admin/reportes/create')}}">--}}
+                                       {{--Nueva Ayuda--}}
+                {{--<i class="fa fa-plus"></i> </a>--}}
+                                        {{--</div>--}}
+                                    {{--</div>--}}
+                                     <hr>
+			<div class="row">
+				<div class="col-md-12">
+
+					<!-- BEGIN EXAMPLE TABLE PORTLET-->
+					<div id="load">
+
+					@if(Session::get('success'))
+					    <div class="alert alert-success">{{ Session::get('success') }}</div>
+                    @endif
+
+					</div>
+					<div class="portlet box blue">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-trophy"></i>Lista de Ayudas
+							</div>
+							<div class="tools">
+							</div>
+						</div>
+
+						<div class="portlet-body">
+
+
+							<table class="table table-striped table-bordered table-hover" id="reportes">
+							<thead>
+							<tr>
+								<th> AyudaID </th>
+								<th> Beneficiario ID </th>
+								<th> Apellido Beneficiario</th>
+								<th> Requerimiento </th>
+								<th> Centro de Salud </th>
+								<th> Nit </th>
+								<th> Nro Factura </th>
+                                <th> Fecha Ayuda </th>
+                                <th> Gastos </th>
+
+
+								<th> Accion </th>
+							</tr>
+							</thead>
+							<tbody>
+                        <tr >
+                                <td>{{-- Hidden ID --}}</td>
+                                <td>{{-- EmployeeID --}}</td>
+                                <td>{{-- Apellido Beneiciario --}}</td>
+                                <td>{{-- Reuqerimiento --}} </td>
+                                <td>{{-- Centro de Salud --}}</td>
+                                <td>{{-- Nit --}}</td>
+                                <td>{{-- Nro Factura --}}</td>
+                                <td>{{-- Fecha Ayuda --}}</td>
+                                <td>{{-- Gastos --}}</td>
+
+                                <td>{{-- Action --}} </td>
+                            </tr>
+
+
+
+							</tbody>
+							</table>
+						</div>
+					</div>
+					<!-- END EXAMPLE TABLE PORTLET-->
+
+				</div>
+			</div>
+			<!-- END PAGE CONTENT-->
+
+			{{--DELETE MODAL CALLING--}}
+                @include('admin.common.delete')
+            {{--DELETE MODAL CALLING END--}}
+@stop
+
+
+
+@section('footerjs')
+
+
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+	{{ HTML::script("assets/global/plugins/select2/select2.min.js")}}
+  {{HTML::script("assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js")}}
+	{{ HTML::script("assets/global/plugins/datatables/media/js/jquery.dataTables.min.js")}}
+	{{ HTML::script("assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js")}}
+
+<!-- END PAGE LEVEL PLUGINS -->
+
+	<script>
+
+
+        	$('#reportes').dataTable( {
+                        "bProcessing": true,
+                        "bServerSide": true,
+                        "sAjaxSource": "{{ route("admin.ajax_reportes") }}",
+                        "aaSorting": [[ 1, "asc" ]],
+                        "aoColumns": [
+                            { 'sClass': 'center', "bSortable": true  },
+                            { 'sClass': 'center', "bSortable": true  },
+                            { 'sClass': 'center', "bSortable": true },
+                            { 'sClass': 'center', "bSortable": true },
+                            { 'sClass': 'center', "bSortable": true },
+                            { 'sClass': 'center', "bSortable": true },
+                            { 'sClass': 'center', "bSortable": true },
+                            { 'sClass': 'center', "bSortable": true },
+                            { 'sClass': 'center', "bSortable": true },
+                            { 'sClass': 'center', "bSortable": false }
+
+
+                        ],
+                        "columnDefs": [
+                                    {
+                                        "targets": [ 0 ],
+                                        "visible": false,
+                                        "searchable": false
+                                    },{
+									  "targets": [ 5 ],
+									  "visible": false,
+									  "searchable": true
+								  }
+                                    ],
+                        "lengthMenu": [
+										[5, 15, 20, -1],
+										[5, 15, 20, "All"] // change per page values here
+									],
+                        "sPaginationType": "full_numbers",
+                        "fnRowCallback": function( nRow, aData, iDisplayIndex ) {
+                            var row = $(nRow);
+                            row.attr("id", 'row'+aData['0']);
+                        }
+
+             });
+
+
+
+		function del(id,ayudaaName,ayuda)
+		{
+
+			$('#deleteModal').appendTo("body").modal('show');
+			$('#info').html('Esta seguro de Eliminar <strong>'+ayuda+'</strong> dado a <strong>'+ayudaaName+'</strong>??');
+			$("#delete").click(function()
+			{
+					var url = "{{ route('admin.ayudas.destroy',':id') }}";
+					url = url.replace(':id',id);
+					 $.ajax({
+
+		                type: "DELETE",
+		                url : url,
+		                dataType: 'json',
+		                data: {"id":id}
+
+		            	}).done(function(response)
+		           		  {
+
+		               	 	 if(response.success == "deleted")
+		                 	 {
+		                 	 		$("html, body").animate({ scrollTop: 0 }, "slow");
+		                  	   		$('#deleteModal').modal('hide');
+		                  	   		 $('#row'+id).fadeOut(500);
+
+		                 	  		$('#load').html("<p class='alert alert-success text-center'><strong>"+name +"</strong> Eliminada exitosamente!</p>");
+		                  	 }
+		           		 });
+				})
+
+			}
+</script>
+@stop
+	
