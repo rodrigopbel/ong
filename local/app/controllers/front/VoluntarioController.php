05806ccb2dc47e3319e8680e27d5ddd249490d44
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-09 17:23:32 -0300

    actualizacion de datos tabla ayudas

--- local/app/controllers/front/VoluntarioController.php
+++ local/app/controllers/front/VoluntarioController.php
@@ -22,8 +22,8 @@ class VoluntarioController extends \BaseController {
             'email'	     =>	$input['email'],
             'tipoPersonal' => $tipo
         ]);
-        return View::make('front.login',$this->data);
-//        return Redirect::route('front.login')->with('success'," Voluntario adicionado");
+//        return View::make('front.login',$this->data);
+        return Redirect::route('front.login')->with('success'," Voluntario adicionado");
     }
 
     
