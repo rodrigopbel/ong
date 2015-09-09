637486c233fb88208881f245947fb512a0e42e8c
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-09 17:50:41 -0400

    report saldo

--- local/app/controllers/admin/ReportesController.php
+++ local/app/controllers/admin/ReportesController.php
@@ -17,9 +17,9 @@ class ReportsController extends \AdminBaseController {
         $this->data['reportesActive'] =   'active';
 
         $this->data['xyz'] = DB::table('ayudas')
-//                            ->join('beneficiarios', 'ayudas.beneficiarioID', '=', 'beneficiarios.beneficiarioID')
-//                            ->join('donaciones', 'ayudas.aportanteID', '=', 'donaciones.aportanteID')
-                            ->select('ayudas.gastos')
+                            ->join('beneficiarios', 'ayudas.beneficiarioID', '=', 'beneficiarios.beneficiarioID')
+                            ->join('donaciones', 'ayudas.aportanteID', '=', 'donaciones.aportanteID')
+                                ->select('ayudas.gastos')
                             ->get();
         echo $this->data['xyz'];
 //            $this->data['ayudas'] = Ayuda::all();
