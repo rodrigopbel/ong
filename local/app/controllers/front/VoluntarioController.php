20e008ddc277f39057452dae2edf5889f51212d6
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-10 19:51:17 -0300

    in-borrar

--- local/app/controllers/front/VoluntarioController.php
+++ local/app/controllers/front/VoluntarioController.php
@@ -10,64 +10,64 @@ class VoluntarioController extends \BaseController {
 
     public function registrar()
     {
-        $validator = Validator::make($input = Input::all(), Voluntario::rules('create'));
-
-        if ($validator->fails())
-        {
-            return Redirect::back()->withErrors($validator)->withInput();
-        }
-
-        DB::beginTransaction();
-        try {
-            $nombres = $input['nombres'];
-            $tipo = 'Voluntario';
-
-            Voluntario::create([
-                'personalID'   => $input['ci'],
-                'nombres'      => ucwords(strtolower($input['nombres'])),
-                'apellidos'    => ucwords(strtolower($input['apellidos'])),
-                'email'         => $input['email'],
-                'telefono'  => $input['telefono'],
-                'tipoPersonal' => $tipo
-            ]);
-
-        }catch(\Exception $e)
-        {
-            DB::rollback();
-            throw $e;
-        }
-
-        DB::commit();
-//        return View::make('front.final',$this->data);
-        return Redirect::route('front.final')->with('success',"<strong>{$nombres}</strong> exitosamente adicionado en le base de datos");
-//    }
-
-
-//        $input  = Input::all();
-//        $rules  =[
-//            'nombres'     => 'required',
-//            'apellidos'   => 'required',
-//            'ci'          => 'required|ci|unique:personal',
-//            'telefono'    => 'required',
-//            'email'	      => 'required|email'
-//        ];
-//        $validator	= Validator::make($input,$rules);
-//        if($validator->fails())
+//        $validator = Validator::make($input = Input::all(), Voluntario::rules('create'));
+//
+//        if ($validator->fails())
 //        {
-//            $output['status'] = 'error';
-//            $output['msg']    =  $validator->getMessageBag()->toArray();
+//            return Redirect::back()->withErrors($validator)->withInput();
 //        }
-//        $tipo = 'Voluntario';
-//        Personal::create([
-//            'nombres'    => $input['nombres'],
-//            'apellidos'  => $input['apellidos'],
-//            'personalID' => $input['ci'],
-//            'telefono'   => $input['telefono'],
-//            'email'	     =>	$input['email'],
-//            'tipoPersonal' => $tipo
-//        ]);
+//
+//        DB::beginTransaction();
+//        try {
+//            $nombres = $input['nombres'];
+//            $tipo = 'Voluntario';
+//
+//            Voluntario::create([
+//                'personalID'   => $input['ci'],
+//                'nombres'      => ucwords(strtolower($input['nombres'])),
+//                'apellidos'    => ucwords(strtolower($input['apellidos'])),
+//                'email'         => $input['email'],
+//                'telefono'  => $input['telefono'],
+//                'tipoPersonal' => $tipo
+//            ]);
+//
+//        }catch(\Exception $e)
+//        {
+//            DB::rollback();
+//            throw $e;
+//        }
+//
+//        DB::commit();
+////
+//        return Redirect::route('front.final')->with('success',"<strong>{$nombres}</strong> exitosamente adicionado en le base de datos");
+////    }
 
-//        return Redirect::route('front.login')->with(" Voluntario adicionado");
+
+        $input  = Input::all();
+        $rules  =[
+            'nombres'     => 'required',
+            'apellidos'   => 'required',
+            'ci'          => 'required|ci|unique:personal',
+            'telefono'    => 'required',
+            'email'	      => 'required|email'
+        ];
+        $validator	= Validator::make($input,$rules);
+        if($validator->fails())
+        {
+            $output['status'] = 'error';
+            $output['msg']    =  $validator->getMessageBag()->toArray();
+        }
+        $tipo = 'Voluntario';
+        Personal::create([
+            'nombres'    => $input['nombres'],
+            'apellidos'  => $input['apellidos'],
+            'personalID' => $input['ci'],
+            'telefono'   => $input['telefono'],
+            'email'	     =>	$input['email'],
+            'tipoPersonal' => $tipo
+        ]);
+
+        return Redirect::route('front.final')->with(" Voluntario adicionado");
     }
 
     
