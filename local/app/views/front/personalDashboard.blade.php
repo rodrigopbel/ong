445653c4f425dfd6232272b035be5f17f601466f
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-10 10:26:03 -0300

    logs

--- local/app/views/front/personalDashboard.blade.php
+++ local/app/views/front/personalDashboard.blade.php
@@ -255,11 +255,13 @@
                             @foreach($donaciones as $donacion)
                             Donacion:
                             {{--@if(strtotime($donacion->date)>time())--}}
-                                        <div class="overflow-h">
-                                            <strong class="color-{{$$donacion_font_color[$donacion->id%count($donacion_font_color)]}}">{{$donacion->monto}}
-                                                <small class="pull-right"><em>{{date('d M Y',strtotime($donacion->created_at))}}</em></small>
-                                            </strong>
-                                       </div>
+                                <div class="alert-blocks alert-blocks-{{$donacion_color[$donacion->id%count($donacion_color)]}}">
+                                    <div class="overflow-h">
+                                        <strong class="color-{{$$donacion_font_color[$donacion->id%count($donacion_font_color)]}}">{{$donacion->monto}}
+                                            <small class="pull-right"><em>{{date('d M Y',strtotime($donacion->created_at))}}</em></small>
+                                        </strong>
+                                    </div>
+                                </div>
                              {{--@endif--}}
                             @endforeach
                         @endif
