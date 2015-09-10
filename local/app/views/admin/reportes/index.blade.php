0f09102d62e463bac9056fd761efcdaf93f7712c
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-10 14:15:32 -0300

    logs

--- local/app/views/admin/reportes/index.blade.php
+++ local/app/views/admin/reportes/index.blade.php
@@ -125,10 +125,7 @@
         $('.generarReporte').on('click', function(){
 //            alert($('#beneficiario').val());
 //            console.log($('#beneficiario').val());
-            var id = $('#beneficiario').val();
-            var data = {
-                'id' : id
-            };
+
             $.ajax({
                 url: '{{route("admin.ajax_reportes")}}',
                 type: 'POST',
