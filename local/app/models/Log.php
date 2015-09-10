33731260ae1001ba0c5f45cca62b26bb94e5a8fc
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-10 13:05:11 -0300

    logs

---
+++ local/app/models/Log.php
@@ -0,0 +1,21 @@
+<?php
+
+class Log extends \Eloquent {
+
+	// Add your validation rules here
+    protected $table = "activity_log";
+//	public static $rules = [
+//		//'ayudaName'      =>  'required',
+//       // 'beneficiarioID' =>  'required',
+//       // 'aportanteID'    =>  'required'
+//
+//	];
+
+	// Don't forget to fill this array
+    protected $guarded = ['id'];
+//    protected $fillable = ['username', 'email', 'password'];
+
+
+
+
+}
