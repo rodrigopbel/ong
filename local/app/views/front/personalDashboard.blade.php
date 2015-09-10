4b4f96e4979d6c54cb1cd607a5d3de6f415fd6b0
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-10 10:40:32 -0300

    logs

--- local/app/views/front/personalDashboard.blade.php
+++ local/app/views/front/personalDashboard.blade.php
@@ -212,8 +212,8 @@
                             @foreach($donaciones as $donacion)
                                 <div class="alert-blocks">
                                     <div class="overflow-h">
-                                        <strong class="color-dark">{{Str::words($donacion->descripcion,1,'')}} <small class="pull-right"><em>{{($donacion->montodonacion)}}</em></small></strong>
-                                        <small class="award-name">{{$donacion->created_at}}</small>
+                                        <strong class="color-dark">{{Str::words($donacion->descripcion,1,'')}} <small class="pull-right"><em>{{($donacion->created_at)}}</em></small></strong>
+                                        <small class="award-name">{{$donacion->montodonacion}}</small>
                                     </div>
                                 </div>
                             @endforeach
@@ -246,28 +246,28 @@
                         </div>
                     </div>
 
-                    <div class="panel panel-profile margin-top-20">
-                        <div class="panel-heading overflow-h">
-                            <h2 class="panel-title heading-sm pull-left"><i class="fa fa-send"></i> Donaciones</h2>
-                        </div>
-                        <div id="scrollbar3" class="panel-body contentHolder">
-                         @if(count($donaciones))
-                            @foreach($donaciones as $donacion)
-                            Donacion:
-                            @if(strtotime($donacion->date)>time())
-                                <div class="alert-blocks alert-blocks-{{$donacion_color[$donacion->id%count($donacion_color)]}}">
-                                    <div class="overflow-h">
-                                        <strong class="color-{{$$donacion_font_color[$donacion->id%count($donacion_font_color)]}}">{{$donacion->montodonacion}}
-                                            <small class="pull-right"><em>{{date('d M Y',strtotime($donacion->created_at))}}</em></small>
-                                        </strong>
-                                    </div>
-                                </div>
-                             @endif
-                            @endforeach
-                        @endif
+                    {{--<div class="panel panel-profile margin-top-20">--}}
+                        {{--<div class="panel-heading overflow-h">--}}
+                            {{--<h2 class="panel-title heading-sm pull-left"><i class="fa fa-send"></i> Donaciones</h2>--}}
+                        {{--</div>--}}
+                        {{--<div id="scrollbar3" class="panel-body contentHolder">--}}
+                         {{--@if(count($donaciones))--}}
+                            {{--@foreach($donaciones as $donacion)--}}
+                            {{--Donacion:--}}
+                            {{--@if(strtotime($donacion->date)>time())--}}
+                                {{--<div class="alert-blocks alert-blocks-{{$donacion_color[$donacion->id%count($donacion_color)]}}">--}}
+                                    {{--<div class="overflow-h">--}}
+                                        {{--<strong class="color-{{$$donacion_font_color[$donacion->id%count($donacion_font_color)]}}">{{$donacion->montodonacion}}--}}
+                                            {{--<small class="pull-right"><em>{{date('d M Y',strtotime($donacion->created_at))}}</em></small>--}}
+                                        {{--</strong>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                             {{--@endif--}}
+                            {{--@endforeach--}}
+                        {{--@endif--}}
 
-                        </div>
-                    </div>
+                        {{--</div>--}}
+                    {{--</div>--}}
 
 
                 </div>
