f94c681fb588898e619100b1bb7ff841f4e3fe9b
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-11 16:38:17 -0300

    don

--- local/app/controllers/admin/DonacionesController.php
+++ local/app/controllers/admin/DonacionesController.php
@@ -27,7 +27,7 @@ class DonacionesController extends \AdminBaseController {
 
 	    $result =
             Donacion::select('donaciones.id','personal.personalID','beneficiarios.nombres','descripcion','montodonacion','donaciones.created_at')
-		      ->join('personal', 'donaciones.aportanteID', '=', 'personal.personalID')
+		        ->join('personal', 'donaciones.aportanteID', '=', 'personal.personalID')
                 ->join('beneficiarios', 'donaciones.aportanteID', '=', 'beneficiarios.beneficiarioID')
 			  ->orderBy('donaciones.created_at','desc');
 
