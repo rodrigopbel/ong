34209e600cc4d30ae428fda35f6742aa739298ae
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-10 21:36:34 -0300

    style report

--- local/app/views/admin/include/head.blade.php
+++ local/app/views/admin/include/head.blade.php
@@ -7,7 +7,7 @@
 {{--{{HTML::style("http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all")}}--}}
 {{HTML::style("assets/global/plugins/font-awesome/css/font-awesome.min.css")}}
 {{HTML::style("assets/global/plugins/simple-line-icons/simple-line-icons.min.css")}}
-{{--{{HTML::style("assets/global/plugins/bootstrap/css/bootstrap.min.css")}}--}}
+{{HTML::style("assets/global/plugins/bootstrap/css/bootstrap.min.css")}}
 {{HTML::style("assets/global/plugins/uniform/css/uniform.default.css")}}
 {{HTML::style("assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css")}}
 
