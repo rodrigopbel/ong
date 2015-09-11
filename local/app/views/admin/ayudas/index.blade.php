98013d35f5b9b443c3b98e17854f78aba50f31c6
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-11 16:24:29 -0300

    act

--- local/app/views/admin/ayudas/index.blade.php
+++ local/app/views/admin/ayudas/index.blade.php
@@ -145,6 +145,7 @@
                             { 'sClass': 'center', "bSortable": true },
                             { 'sClass': 'center', "bSortable": true },
                             { 'sClass': 'center', "bSortable": true },
+                            { 'sClass': 'center', "bSortable": true },
                             { 'sClass': 'center', "bSortable": false }
 
 
