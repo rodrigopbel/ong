17f27929d0816d533982ca2d5613d09aa2b72eb3
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-14 10:43:56 -0400

    Commit Retroceso

--- local/app/controllers/admin/BeneficiariosController.php
+++ local/app/controllers/admin/BeneficiariosController.php
@@ -85,18 +85,8 @@ class BeneficiariosController extends \AdminBaseController {
                 'direccion'  => $input['direccion'],
                 'foto'  =>  isset($filename)?$filename:'default.jpg',
                 'fechaing'   =>  date('Y-m-d',strtotime($input['fechaing'])),
-                'direccionperm' => $input['direccionperm'],
-                'iddiagnostico'  => $input['iddiagnostico'],
-                'diagnostico'  => $input['diagnostico'],
-                'fechadiagnostico'  => $input['fechadiag'],
-                'tratamiento'  => $input['tratamiento'],
-                'razon'  => $input['razon'],
-                'duracion'  => $input['duracion'],
-                'referencia'  => $input['referencia'],
-                'lugar'  => $input['lugar']
+                'direccionperm' => $input['direccionperm']
             ]);
-
-            dd("abajo del create");
             //  Insert into salary table
             if ($input['montosolicitado'] != '')
             {
