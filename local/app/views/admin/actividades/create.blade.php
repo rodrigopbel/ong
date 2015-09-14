83c4ab11c43420e092e21bfadb417a749ac1834f
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-14 15:30:53 -0300

    new activity

---
+++ local/app/views/admin/actividades/create.blade.php
@@ -0,0 +1,118 @@
+@extends('admin.adminlayouts.adminlayout')
+
+@section('head')
+    <!-- BEGIN PAGE LEVEL STYLES -->
+    {{HTML::style("assets/global/plugins/bootstrap-select/bootstrap-select.min.css")}}
+    {{HTML::style("assets/global/plugins/select2/select2.css")}}
+    {{HTML::style("assets/global/plugins/jquery-multi-select/css/multi-select.css")}}
+    <!-- BEGIN THEME STYLES -->
+@stop
+
+
+@section('mainarea')
+
+			
+			<!-- BEGIN PAGE HEADER-->
+			<h3 class="page-title">
+			Ayuda
+			</h3>
+			<div class="page-bar">
+				<ul class="page-breadcrumb">
+					<li>
+						<i class="fa fa-home"></i>
+						<a href="{{route('admin.dashboard.index')}}">Inicio</a>
+						<i class="fa fa-angle-right"></i>
+					</li>
+					<li>
+						<a href="{{ route('admin.actividades.index') }}">Actividad</a>
+						<i class="fa fa-angle-right"></i>
+					</li>
+					<li>
+						<a href="">Nueva Actividad</a>
+					</li>
+				</ul>
+			
+			</div>
+			<!-- END PAGE HEADER-->
+			<!-- BEGIN PAGE CONTENT-->
+			<div class="row">
+				<div class="col-md-12">
+					<!-- BEGIN EXAMPLE TABLE PORTLET-->
+
+                {{--INLCUDE ERROR MESSAGE BOX--}}
+                      @include('admin.common.error')
+                {{--END ERROR MESSAGE BOX--}}
+
+
+					<div class="portlet box blue">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-trophy"></i>Nueva Actividad
+							</div>
+							<div class="tools">
+							</div>
+						</div>
+
+						<div class="portlet-body form">
+
+						<!-- BEGIN FORM-->
+						{{Form::open(array('url'=>"admin/actividades",'class'=>'form-horizontal form-bordered','method'=>'POST'))}}
+
+
+                                    <div class="form-body">
+
+
+                                        <div class="form-group">
+                                            <label class="col-md-2 control-label">Fecha de Actividad: <span class="required">
+                                            * </span>
+                                            </label>
+                                            <div class="col-md-6">
+                                                <input type="text" class="form-control" name="fechaAct" placeholder="Requerimiento" value="{{ Input::old('fechaAct') }}">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="col-md-2 control-label">Descripcion: <span class="required">
+                                        * </span>
+                                            </label>
+                                            <div class="col-md-6">
+                                                <input type="text" class="form-control" name="descripcion" placeholder="Centro de Salud" value="{{ Input::old('descripcion') }}">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-md-2 control-label">Lugar: <span class="required">
+                                        * </span>
+                                            </label>
+                                            <div class="col-md-6">
+                                                <input type="text" class="form-control" name="lugar" placeholder="Nit" value="{{ Input::old('lugar') }}">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-actions">
+                                            <div class="row">
+                                                <div class="col-md-offset-3 col-md-9">
+                                                    <button type="submit" data-loading-text="Actualizando..." class="demo-loading-btn btn green"><i class="fa fa-check"></i> Guardar</button>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                {{ Form::close() }}
+                                    <!-- END FORM-->
+						</div>
+					</div>
+					<!-- END EXAMPLE TABLE PORTLET-->
+					
+				</div>
+			</div>
+			<!-- END PAGE CONTENT-->
+
+@stop
+
+@section('footerjs')
+
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+{{HTML::script("assets/global/plugins/bootstrap-select/bootstrap-select.min.js")}}
+{{HTML::script("assets/global/plugins/select2/select2.min.js")}}
+{{HTML::script("assets/global/plugins/jquery-multi-select/js/jquery.multi-select.js")}}
+<!-- END PAGE LEVEL PLUGINS -->
+@stop
