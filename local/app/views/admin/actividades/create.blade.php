20a6306c533d42b98f1139b5dec96ab4d6f499e3
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-14 16:36:47 -0300

    new activity

--- local/app/views/admin/actividades/create.blade.php
+++ local/app/views/admin/actividades/create.blade.php
@@ -119,6 +119,7 @@
 {{HTML::script("assets/global/plugins/bootstrap-select/bootstrap-select.min.js")}}
 {{HTML::script("assets/global/plugins/select2/select2.min.js")}}
 {{HTML::script("assets/global/plugins/jquery-multi-select/js/jquery.multi-select.js")}}
+                    {{HTML::script('assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.js')}}
                     {{HTML::script('assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js')}}
                     {{HTML::script("assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js")}}
                     {{HTML::script('assets/admin/pages/scripts/components-pickers.js')}}
