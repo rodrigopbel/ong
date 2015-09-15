6a3ee8910d9cebfd2214827c0c0bcd1ec3edcced
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-15 11:32:09 -0400

    Actividades calendar

--- local/app/controllers/admin/ParticipacionesController.php
+++ local/app/controllers/admin/ParticipacionesController.php
@@ -185,6 +185,7 @@ class ParticipacionesController extends \AdminBaseController {
         $input = Input::all();
         $participacion = new Participacion;
         $participacion->actividadID     = $input['idActividad'];
+        echo ($input);
         foreach($input['participantes'] as $index => $value)
         {
             echo $value;
