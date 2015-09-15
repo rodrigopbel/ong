f005b309c531f83376e47582100c1bbaed8e5323
GuillermoParedes <gdavid.ptorrez@gmail.com>
2015-09-15 00:29:57 -0400

    participacion asistencia

--- local/app/views/admin/participaciones/index.blade.php
+++ local/app/views/admin/participaciones/index.blade.php
@@ -76,7 +76,7 @@
                                 <td> {{ $act->actividadID }} </td>
                                 <td class="">
                                     <a class="btn purple"
-                                       href="{{route('admin.participaciones.show',$act->id) }}">
+                                       href="{{route('admin.participaciones.edit',$act->id) }}">
                                         <i class="fa fa-eye"></i> Agregar Asistentes
                                     </a>
                                 </td>
