c7b07dfa3648eea8a3f2eef1312e09a93a9f7e5a
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-15 11:04:57 -0300

    new activity

--- local/app/views/admin/actividades/edit.blade.php
+++ local/app/views/admin/actividades/edit.blade.php
@@ -61,29 +61,18 @@
 
                                     <div class="form-body">
 
+
                                         <div class="form-group">
-                                            <label class="control-label col-md-3">Fecha de Diagnostico</label>
+                                            <label class="control-label col-md-3">Fecha de Actividad</label>
                                             <div class="col-md-3">
                                                 <div class="input-group input-medium date date-picker"  data-date-format="dd-mm-yyyy" data-date-viewmode="years">
-                                                    <input type="text" class="form-control" name="fechaAct" readonly value="{{$actividad->fechaAct}}">
+                                                    <input type="text" class="form-control" name="fechaAct" readonly value="@if(empty($actividad->fechaAct))@else{{date('d-m-Y',strtotime($actividad->fechaAct))}}@endif" >
         												<span class="input-group-btn">
         												<button class="btn default" type="button"><i class="fa fa-calendar"></i></button>
         												</span>
                                                 </div>
                                             </div>
                                         </div>
-
-                                        {{--<div class="form-group">--}}
-                                            {{--<label class="control-label col-md-3">Fecha de Actividad</label>--}}
-                                            {{--<div class="col-md-3">--}}
-                                                {{--<div class="input-group input-medium date date-picker"  data-date-format="dd-mm-yyyy" data-date-viewmode="years">--}}
-                                                    {{--<input type="text" class="form-control" name="fechaAct" readonly value="@if(empty($actividad->fechaAct))@else{{date('d-m-Y',strtotime($actividad->fechaAct))}}@endif" >--}}
-        												{{--<span class="input-group-btn">--}}
-        												{{--<button class="btn default" type="button"><i class="fa fa-calendar"></i></button>--}}
-        												{{--</span>--}}
-                                                {{--</div>--}}
-                                            {{--</div>--}}
-                                        {{--</div>--}}
                                         <div class="form-group">
                                             <label class="col-md-2 control-label">Descripcion: </label>
                                             <div class="col-md-6">
@@ -136,4 +125,122 @@
                     {{HTML::script("assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js")}}
                     {{HTML::script('assets/admin/pages/scripts/components-pickers.js')}}
 <!-- END PAGE LEVEL PLUGINS -->
+
+
+                    <script>
+                        jQuery(document).ready(function() {
+                            ComponentsPickers.init();
+                            objetivos();
+
+                        });
+
+                        function objetivos(){
+
+                            $.getJSON("{{ URL::to('admin/destinos/ajax_objetivos/')}}",
+                                    { destID: $('#destino').val() },
+                                    function(data) {
+                                        var model = $('#objetivo');
+                                        model.empty();
+                                        var selected='';
+                                        var match= {{ $beneficiario->objetivo}}
+                                                $.each(data, function(index, element) {
+                                                    model.append("<option value='"+element.id+"'>" + element.objetivo + "</option>");
+                                                });
+
+                                    });
+
+                        }
+
+                        // Javascript function to update the company info and Bank Info
+                        function UpdateDetails(id,type){
+
+                            var  form_id = '';
+                            var alert_div = '';
+
+                            if(type=='zonificacion')
+                            {
+                                form_id     = '#bank_details_form';
+                                alert_div   =  '#alert_bank'
+
+                            }else
+                            {
+                                form_id     = '#company_details_form';
+                                alert_div   =   '#alert_company';
+                            }
+                            $(alert_div).html('<div class="alert alert-info"><span class="fa fa-info"></span> Guardando..</div>');
+                            var url = "{{ route('admin.beneficiarios.update',':id') }}";
+                            url = url.replace(':id',id);
+                            $.ajax({
+                                type: "PATCH",
+                                url : url,
+                                dataType: 'json',
+                                data: $(form_id).serialize()
+
+                            }).done( function( response ) {
+                                $(alert_div).html('');
+                                if(response.status == "success")
+                                {
+                                    $(alert_div).html('<div class="alert alert-success alert-dismissable"><button class="close" data-close="alert"></button><span class="fa fa-check"></span> '+response.msg+'</div>');
+
+                                }else if(response.status == "error")
+                                {
+                                    var arr = response.msg;
+                                    var alert ='';
+                                    $.each(arr, function(index, value)
+                                    {
+                                        if (value.length != 0)
+                                        {
+                                            alert += '<p><span class="fa fa-warning"></span> '+ value+ '</p>';
+
+                                        }
+                                    });
+
+                                    $(alert_div).append('<div class="alert alert-danger alert-dismissable"><button class="close" data-close="alert"></button> '+alert+'</div>');
+                                }
+                            });
+                        }
+
+                        function del(id,type)
+                        {
+
+                            var alert_div   =   '#alert_company';
+                            $('#deleteModal').appendTo("body").modal('show');
+                            $('#info').html('Eliminar  <strong>'+type+'</strong> Donacion??.');
+                            $("#delete").click(function()
+                            {
+                                var url = "{{ route('admin.salary.destroy',':id') }}";
+                                url = url.replace(':id',id);
+                                $.ajax({
+
+                                    type: "DELETE",
+                                    url : url,
+                                    dataType: 'json',
+                                    data: {"id":id}
+
+                                }).done(function(response)
+                                {
+                                    if(response.success == "deleted")
+                                    {
+                                        $('#deleteModal').modal('hide');
+                                        $('#salary'+id).remove();
+                                        $(alert_div).html('<div class="alert alert-success alert-dismissable"><button class="close" data-close="alert"></button><span class="fa fa-check"></span> '+response.msg+'</div>');
+                                    }
+                                });
+                            })
+
+                        }
+
+                        function remove_exit()
+                        {
+                            if($("input[name=status]:checked").val() == "active"){
+                                $("input[name=exit_date]").val("");
+                            }
+                        }
+
+
+                        $("input[name=exit_date]").change(function () {
+                            $("input[name=status]").bootstrapSwitch('state',false);
+
+                        });
+                    </script>
 @stop
