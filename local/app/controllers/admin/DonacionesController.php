846f124c457520266d4884a46dea281e867e402e
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-15 14:07:34 -0300

    new activity

--- local/app/controllers/admin/DonacionesController.php
+++ local/app/controllers/admin/DonacionesController.php
@@ -38,7 +38,7 @@ class DonacionesController extends \AdminBaseController {
                         <a  class="btn purple"  href="{{ route(\'admin.donaciones.edit\',$id)}}" ><i class="fa fa-edit"></i></a>
                             &nbsp;<a href="javascript:;" onclick="del(\'{{ $id }}\',\'{{ $descripcion}}\',\'{{ $montodonacion }}\');return false;" class="btn red">
                         <i class="fa fa-trash"></i></a>')
-//            ->remove_column('created_at')
+            ->remove_column('updated_at')
             ->make();
     }
 
