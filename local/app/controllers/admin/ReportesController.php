5bb19c924a87d84befd6aa1dd3189ed852d473bd
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-15 13:29:11 -0400

    Actividades calendar

--- local/app/controllers/admin/ReportesController.php
+++ local/app/controllers/admin/ReportesController.php
@@ -12,6 +12,7 @@ class ReportsController extends \AdminBaseController {
     //    Display a listing of awards
     public function index()
     {
+        dd("entro aqui");
         $this->data['reportesActive'] =   'active';
         $this->data['beneficiarios'] = Beneficiario::selectRaw('CONCAT(apellidos, " (ID:", beneficiarioID,")") as apellidos, beneficiarioID')
             ->where('status','=','activo')
