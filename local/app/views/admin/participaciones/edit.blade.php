c4e0e869b370072979f7f6c53012efc29b95ff80
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-15 08:31:06 -0400

    Actividades calendar

--- local/app/views/admin/participaciones/edit.blade.php
+++ local/app/views/admin/participaciones/edit.blade.php
@@ -7,7 +7,7 @@
     <!-- BEGIN THEME STYLES -->
 @stop
 
-{{dd("entro a la templeta")}}
+
 @section('mainarea')
 
 
@@ -19,15 +19,15 @@
         <ul class="page-breadcrumb">
             <li>
                 <i class="fa fa-home"></i>
-                <a href="{{route('admin.dashboard.index')}}">Inicio</a>
+                <a href="{{route('admin.dashboard.index')}}">Home</a>
                 <i class="fa fa-angle-right"></i>
             </li>
             <li>
-                <a href="{{ route('admin.participaciones.index') }}">Participaciones</a>
+                <a href="{{ route('admin.attendances.index') }}">Attendace</a>
                 <i class="fa fa-angle-right"></i>
             </li>
             <li>
-                <a href="">Asistencia</a>
+                <a href="">Update attendace</a>
             </li>
         </ul>
 
@@ -41,162 +41,182 @@
             {{--INLCUDE ERROR MESSAGE BOX--}}
             @include('admin.common.error')
             {{--END ERROR MESSAGE BOX--}}
-            <hr>
-            <div class="portlet box blue">
-                <div class="portlet-title">
-                    <div class="caption">
-                        <i class="fa fa-edit"></i>
-                        @if(isset($activiad->created_at))
-                            Fecha , {{date('d M Y',strtotime($activiad->created_at))}}
-                        @else
-                           Rayos
-                        @endif
-                    </div>
-                    <div class="tools">
-                    </div>
-                </div>
 
-                <div class="portlet-body form">
-                                <!-- BEGIN FORM-->
-                        {{Form::open(['route'=>["admin.participaciones.update",$activiad->created_at],'class'=>'form-horizontal','method'=>'PATCH'])}}
+            <div class="row">
 
 
-                        <div class="form-body">
-
-                            <h3 class="form-section">Fecha  {{date('d-M-Y',strtotime($activiad->created_at))}}</h3>
-
-                            <div class="form-group">
-
-                                <label class="col-md-1 control-group">Voluntario ID</label>
-                                <label class="col-md-2 control-group">Nombres</label>
-                                <label class="col-md-2 control-group">Apellidos</label>
-                                <label class="col-md-2 control-group">Telefono</label>
-                                <label class="col-md-2 control-group">Email</label>
-                                <label class="col-md-2 control-group">Partipacion</label>
-                            </div>
-
-                            @foreach($voluntarios as $voluntario)
-                                <div class="form-group">
-                                    <label class="col-md-1 control-group">{{$voluntario->personalID}} </label>
-                                    <label class="col-md-2 control-group">{{$voluntario->nombres}} </label>
-                                    <label class="col-md-2 control-group">{{$voluntario->apellidos}} </label>
-                                    <label class="col-md-2 control-group">{{$voluntario->telefono}} </label>
-                                    <label class="col-md-2 control-group">{{$voluntario->email}} </label>
-
-                                    <div class="col-md-2">
-                                        <input type="checkbox" id="checkbox{{$voluntario->personalID}}"
-                                               onchange="showHide('{{$voluntario->personalID}}');return false;"
-                                               class="make-switch" name="checkbox[{{$voluntario->personalID}}]" checked
-                                               data-on-color="success" data-on-text="P" data-off-text="A"
-                                               data-off-color="danger">
-                                        <input type="hidden" name="employees[]" value="{{$voluntario->personalID}}">
-                                    </div>
-
-
-                                </div>
-                            @endforeach
-
-                            <div class="form-actions">
-                                <div class="row">
-                                    <div class="col-md-offset-3 col-md-9">
-                                        <button type="submit" data-loading-text="Submitting..."
-                                                class="demo-loading-btn btn green"><i class="fa fa-edit"></i> Guardar
-                                        </button>
-
-                                    </div>
-                                </div>
-                            </div>
-                            {{ Form::close() }}
-
+                <hr>
+                <div class="portlet box blue">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="fa fa-edit"></i>
+                            @if(isset($todays_holidays->date))
+                                Holiday , {{date('d M Y',strtotime($todays_holidays->date))}}
+                            @else
+                                Mark Attendance
                             @endif
-                                    <!-- END FORM-->
-
                         </div>
+                        <div class="tools">
+                        </div>
+                    </div>
+
+                    <div class="portlet-body form">
+
+                        @if(isset($todays_holidays->date))
+                            <div class="note note-info">
+                                <h4 class="block">{{date('D', strtotime($todays_holidays->date))}}</h4>
+
+                                <p>
+                                    Holiday on the occassion of {{ $todays_holidays->occassion }}
+                                </p>
+                            </div>
+                        @elseif(count($employees)==0)
+                            <hr>
+                            <div class="note note-warning">
+                                <h4 class="block">Employees Missing</h4>
+
+                                <p>
+                                    Please add some employees to the database
+                                </p>
+                            </div>
+                            <hr>
+                            @else
+                                    <!-- BEGIN FORM-->
+                            {{Form::open(['route'=>["admin.partipaciones.update",$date],'class'=>'form-horizontal','method'=>'PATCH'])}}
+
+
+                            <div class="form-body">
+
+                                <h3 class="form-section">Date  {{date('d-M-Y',strtotime($date))}}</h3>
+
+                                <div class="form-group">
+
+                                    <label class="col-md-1 control-group">EmployeeID</label>
+                                    <label class="col-md-2 control-group">Name</label>
+                                    <label class="col-md-2 control-group">Status </label>
+                                    <label class="col-md-2 control-group leaveType" id="leaveTypeLabel">Type of
+                                        leave </label>
+                                    <label class="col-md-2 control-group"><span class="halfLeaveType" id="halfDayLabel">half Day leave type</span>
+                                    </label>
+
+                                    <label class="col-md-3 control-group reason" id="reasonLabel">Reason </label>
+
+                                </div>
+
+                                @foreach($voluntarios as $vol)
+                                    <div class="form-group">
+                                        <label class="col-md-1 control-group">{{$vol->personalID}} </label>
+                                        <label class="col-md-2 control-group">{{$vol->nombres}} {{$vol->apellidos}} </label>
+
+                                        <div class="col-md-2">
+                                            <input type="checkbox" id="checkbox{{$vol->personalID}}"
+                                                   onchange="showHide('{{$vol->personalID}}');return false;"
+                                                   class="make-switch" name="checkbox[{{$vol->personalID}}]"
+                                                   checked data-on-color="success" data-on-text="P" data-off-text="A"
+                                                   data-off-color="danger">
+                                            <input type="hidden" name="employees[]" value="{{$vol->personalID}}">
+                                        </div>
+                                        <div class="col-md-2">
+                                            @if(isset($attendanceArray[$employee->employeeID]['leaveType']))
+                                                {{ Form::select('leaveType['.$employee->employeeID.']', $leaveTypes,$attendanceArray[$employee->employeeID]['leaveType'],['class' => 'form-control leaveType','onchange'=>'halfDayToggle('.$employee->employeeID.',this.value)','id'=>'leaveType'.$employee->employeeID.''] ) }}
+                                            @else
+                                                {{ Form::select('leaveType['.$employee->employeeID.']', $leaveTypes,null,['class' => 'form-control leaveType','onchange'=>'halfDayToggle('.$employee->employeeID.',this.value)','id'=>'leaveType'.$employee->employeeID.''] ) }}
+                                            @endif
+                                        </div>
+                                        <div class="col-md-2">
+                                            @if(isset($attendanceArray[$employee->employeeID]['leaveType']))
+                                                {{ Form::select('leaveTypeWithoutHalfDay['.$employee->employeeID.']', $leaveTypeWithoutHalfDay,$attendanceArray[$employee->employeeID]['halfDayType'],['class' => 'form-control halfLeaveType','id'=>'halfLeaveType'.$employee->employeeID.''] ) }}
+                                            @else
+                                                {{ Form::select('leaveTypeWithoutHalfDay['.$employee->employeeID.']', $leaveTypeWithoutHalfDay,null,['class' => 'form-control halfLeaveType','id'=>'halfLeaveType'.$employee->employeeID.''] ) }}
+                                            @endif
+                                        </div>
+                                        <div class="col-md-3">
+                                            <input type="text" class="form-control reason"
+                                                   id="reason{{$employee->employeeID}}"
+                                                   name="reason[{{$employee->employeeID}}]" placeholder="Absent Reason"
+                                                   value="{{ $attendanceArray[$employee->employeeID]['reason'] or ''}}">
+                                        </div>
+                                    </div>
+                                @endforeach
+
+                                <div class="form-actions">
+                                    <div class="row">
+                                        <div class="col-md-offset-3 col-md-9">
+                                            <button type="submit" data-loading-text="Submitting..."
+                                                    class="demo-loading-btn btn green"><i class="fa fa-edit"></i> Submit
+                                            </button>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                {{ Form::close() }}
+
+                                @endif
+                                        <!-- END FORM-->
+
+                            </div>
+                    </div>
+                    <!-- END EXAMPLE TABLE PORTLET-->
+
                 </div>
-                <!-- END EXAMPLE TABLE PORTLET-->
-
             </div>
-        </div>
-        <!-- END PAGE CONTENT-->
+            <!-- END PAGE CONTENT-->
 
 
 
-        @stop
+            @stop
 
-        @section('footerjs')
+            @section('footerjs')
 
-            <!-- BEGIN PAGE LEVEL PLUGINS -->
-            {{HTML::script("assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js")}}
-            {{HTML::script("assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js")}}
-            {{HTML::script("assets/admin/pages/scripts/components-pickers.js")}}
+                <!-- BEGIN PAGE LEVEL PLUGINS -->
+                {{HTML::script("assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js")}}
+                {{HTML::script("assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js")}}
+                {{HTML::script("assets/admin/pages/scripts/components-pickers.js")}}
 
-            <!-- END PAGE LEVEL PLUGINS -->
+                <!-- END PAGE LEVEL PLUGINS -->
 
-            <script>
-                jQuery(document).ready(function () {
-                    ComponentsPickers.init();
+                <script>
+                    jQuery(document).ready(function () {
+                        ComponentsPickers.init();
 
-                });
-            </script>
-            <script>
+                    });
+                </script>
+                <script>
 
-                $('.leaveType').hide();
-                $('.reason').hide();
-                $('.halfLeaveType').hide();
-
-
-                {{--@foreach($attendanceArray as $attend)--}}
-                {{--{--}}
-                    {{--@if($attend['status']=='absent')--}}
-                    {{--$('#leaveTypeLabel').show(100);--}}
-                    {{--$('#reasonLabel').show(100);--}}
-
-                    {{--@if($attend['leaveType']=='half day')--}}
-                    {{--$("#halfLeaveType{{$attend['employeeID']}}").show();--}}
-                    {{--@endif--}}
-                        {{--@if($attend['halfDayType']	!=null)--}}
-                    {{--$('#halfDayLabel').show(100);--}}
-                    {{--@endif--}}
-
-                {{--$("#checkbox{{$attend['employeeID']}}").bootstrapSwitch('state', false);--}}
-
-                    {{--@else--}}
-                         {{--$("#reason{{$attend['employeeID']}}").hide();--}}
-                    {{--$("#leaveType{{$attend['employeeID']}}").hide();--}}
-                    {{--$("#halfLeaveType{{$attend['employeeID']}}").hide();--}}
-                    {{--@endif--}}
+                    $('.leaveType').hide();
+                    $('.reason').hide();
+                    $('.halfLeaveType').hide();
 
 
-                {{--}--}}
-                {{--@endforeach--}}
-              function showHide(id) {
-                    $('#leaveTypeLabel').show(100);
-                    $('#reasonLabel').show(100);
+
+                  function showHide(id) {
+                        $('#leaveTypeLabel').show(100);
+                        $('#reasonLabel').show(100);
 
 
-                    if ($('#checkbox' + id + ':checked').val() == 'on') {
-                        $('#leaveType' + id).hide(1000);
-                        $('#reason' + id).hide(1000);
+                        if ($('#checkbox' + id + ':checked').val() == 'on') {
+                            $('#leaveType' + id).hide(1000);
+                            $('#reason' + id).hide(1000);
 
-                    } else {
-                        $('#leaveType' + id).show(100);
+                        } else {
+                            $('#leaveType' + id).show(100);
 
-                        $('#reason' + id).show(500);
+                            $('#reason' + id).show(500);
+                        }
                     }
-                }
 
-                function halfDayToggle(id, value) {
+                    function halfDayToggle(id, value) {
 
-                    if (value == 'half day') {
-                        $('#halfDayLabel').show(100);
-                        $('#halfLeaveType' + id).show(100);
-                    } else {
-                        $('#halfLeaveType' + id).hide(100);
+                        if (value == 'half day') {
+                            $('#halfDayLabel').show(100);
+                            $('#halfLeaveType' + id).show(100);
+                        } else {
+                            $('#halfLeaveType' + id).hide(100);
+                        }
+
+
                     }
 
 
-                }
-
-
-            </script>
+                </script>
 @stop
