855b452d73f8b27f90869e7f1d1864da9200b3a4
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-16 15:30:12 -0400

    Todo funcionando

--- local/app/controllers/front/DashboardController.php
+++ local/app/controllers/front/DashboardController.php
@@ -21,7 +21,8 @@ class DashboardController extends \BaseController {
 //        $this->data['beneficiarios']   =    Beneficiario::all();
 
         $this->data['beneficiarios']      =    Beneficiario::where('beneficiarioID', '=',$this->data['beneficiarioID']  )->get()->first();
-        $this->data['bbeneficiarios']      =    Beneficiario::where('beneficiarioID','=','90870')->get();
+//        $this->data['bbeneficiarios']      =    Beneficiario::where('beneficiarioID','=','90870')->get();
+        $this->data['bbeneficiarios']      =    Beneficiario::where('beneficiarioID','=',$this->data['beneficiarioID'])->get();
 
 //                                                        where('aportanteID', '=', Auth::personales()->get()->personalID)->get();
 //                                                        ->get();
