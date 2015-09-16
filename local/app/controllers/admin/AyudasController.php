0d44a4a3326e2dd8b561b003bced56559251191c
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-16 10:00:50 -0400

    Todo funcionando

--- local/app/controllers/admin/AyudasController.php
+++ local/app/controllers/admin/AyudasController.php
@@ -83,7 +83,7 @@ class AyudasController extends \AdminBaseController {
 
         $donacion = Donacion::where('aportanteID','=',$input['personalID'])->get()->first();
         $beneficiario = Beneficiario::where('beneficiarioID', '=', $input['beneficiarioID']);
-        dd("asdasd");
+        dd($beneficiario->nombres);
         Saldo::create([
             'nombreBeneficiario' => $beneficiario->nombres,
             'donacionesID'  =>  $donacion->id,
