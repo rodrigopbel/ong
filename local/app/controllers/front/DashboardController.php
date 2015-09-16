f16390ec12ac5447fae2aaeeadc9d3f429aa61a6
Viviana Fernandez <viviana.f8a@gmail.com>
2015-09-16 10:21:36 -0300

    new activity

--- local/app/controllers/front/DashboardController.php
+++ local/app/controllers/front/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends \BaseController {
         $this->data['personal']        =    Personal::find(Auth::personales()->get()->id);
         $this->data['donaciones']      =    Donacion::where('aportanteID', '=', Auth::personales()->get()->personalID)->get();
         $this->data['ayudas']          =    Ayuda::where('aportanteID', '=', Auth::personales()->get()->personalID)->get();
-        $this->data['beneficiarios']   =    Beneficiario::where('beneficiarioID', '=', $this->data['ayudas'])->get();
+        $this->data['beneficiarios']   =    Beneficiario::where('beneficiarioID', '=', $this->data['ayudas']->beneficiarioID)->get();
 
 //                                                        where('aportanteID', '=', Auth::personales()->get()->personalID)->get();
 //                                                        ->get();
