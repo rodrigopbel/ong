e27d874f7efed73e8de04dc8a70b6c0f126b1c56
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-18 15:42:51 -0400

    Eliminacion de Bugs

--- local/app/controllers/front/DashboardController.php
+++ local/app/controllers/front/DashboardController.php
@@ -34,120 +34,7 @@ class DashboardController extends \BaseController {
         return View::make('front.personalDashboard',$this->data);
 	}
 
-//	show leave Page
-	public function leave()
-	{
-        $this->data['leaveActive'] =    'active';
 
-//        $this->data['attendance']         = Attendance::where('employeeID', '=',  $this->data['employeeID'] )->get();
-
-        return View::make('front.leave',$this->data);
-	}
-
-
-	public function  notice_ajax($id)
-	{
-        $notice                   =    Noticeboard::find($id);
-        $output['title']          =   $notice->title;
-        $output['description']    =   $notice->description;
-
-        return Response::json($output,200);
-	}
-
-    //	Submitting the leave request from Employee
-	public function leave_store()
-    {
-
-        $input = Input::all();
-
-        if ($input['date'][0] == '') {
-            Session::flash('error_leave', ['<strong>Error!</strong> Please select the date']);
-            return Redirect::route('front.leave');
-        }
-
-        foreach ($input['date'] as $index => $value) {
-            if (empty($value)) continue;
-            try {
-
-                Attendance::create([
-                    'employeeID' => $this->data['employeeID'],
-                    'date' => date('Y-m-d', strtotime($value)),
-                    'status' => 'absent',
-                    'leaveType' => $input['leaveType'][$index],
-                    'halfDayType' => ($input['leaveType'][$index]=='half day')?$input['halfleaveType'][$index]:null,
-                    'reason' =>   $input['reason'][$index],
-                    'application_status' => 'pending',
-                    'applied_on' => date('Y-m-d', time())
-                ]);
-
-                $this->data['dates'][$index] = date('d-M-Y', strtotime($value));
-                $this->data['leaveType'][$index] = $input['leaveType'][$index];
-                $this->data['reason'][$index] = $input['reason'][$index];
-
-            } catch (Exception $e) {
-
-                Session::flash('error_leave', ['<strong>Error!</strong> You have already applied leave for the particular date']);
-                return Redirect::route('front.leave');
-            }
-
-        }
-
-//        Send email to all admins
-        $admins = Admin::select('email')->get()->toArray();
-        foreach ($admins as $admin){
-            Mail::send('emails.leave_request', $this->data, function ($message) use ($admin) {
-                $message->from(Auth::personales()->get()->email, Auth::personales()->get()->fullName);
-                $message->to($admin['email'])
-                    ->subject('Leave Request - ' . $this->data['setting']->website);
-            });
-        }
-        Session::flash('success_leave','<strong>Success!</strong> Leave request is send to the HR Manger.You will be notified soon.');
-        return Redirect::route('front.leave');
-
-
-    }
-
-
-    //Datatable ajax request
-    public function ajaxApplications()
-    {
-
-        $result = Attendance::select('id','date','leaveType','reason','applied_on','application_status','halfDayType')
-            ->where('employeeID','=',$this->data['employeeID'])
-            ->whereNotNull('application_status')
-            ->orderBy('applied_on','desc');
-
-        return Datatables::of($result)
-            ->edit_column('date',function($row){
-                return date('d-M-Y',strtotime($row->date));
-            })
-            ->edit_column('applied_on',function($row){
-                return date('d-M-Y',strtotime($row->applied_on));
-            })
-	        ->edit_column('leaveType',function($row){
-		        $leave = ($row->leaveType=='half day')?$row->leaveType.'-'.$row->halfDayType:$row->leaveType;
-		        return $leave;
-	        })
-            ->edit_column('reason',function($row){
-	            return    strip_tags(Str::limit($row->reason,50));
-
-            })
-            ->edit_column('application_status',function($row)
-            {
-                $color = [
-                    'pending'   =>  'warning',
-                    'approved'  =>  'success',
-                    'rejected'  =>  'danger'
-                ];
-
-                return "<span class='label label-{$color[$row->application_status]}'>{$row->application_status}</span>";
-            })
-	        ->remove_column('halfDayType')
-            ->add_column('edit', '
-                        <button  class="btn-u btn-u-blue" data-toggle="modal" data-target=".show_notice" onclick="show_application({{ $id }});return false;" ><i class="fa fa-eye"></i> View</button>
-                         ')
-            ->make();
-    }
 
 	public  function changePasswordModal()
 	{
@@ -189,13 +76,7 @@ class DashboardController extends \BaseController {
 
     }
 
-// Ajax leave application view show
-    public function show($id)
-    {
 
-        $this->data['leave_application']    =   Attendance::find($id);
-       return View::make('front.leave_modal_show',$this->data);
-    }
 
 
 
