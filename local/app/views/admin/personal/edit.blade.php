af4a7ad55fc6f56c429825c0980a6ef438d18b1a
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-18 16:25:36 -0400

    Eliminacion de Bugs

--- local/app/views/admin/personal/edit.blade.php
+++ local/app/views/admin/personal/edit.blade.php
@@ -163,7 +163,7 @@
                             <div class="col-md-9">
                                 <label for="">{{$personal->tipoPersonal}}</label>
                             </div>
-                        </div>>
+                        </div>
                         <div class="form-group">
                             <label class="col-md-3 control-label">Telefono</label>
 
