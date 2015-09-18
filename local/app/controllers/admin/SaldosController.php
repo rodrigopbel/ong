8641df651689a03f85c85079863c8f4185f8b60a
ChiGY <gdavid.ptorrez@gmail.com>
2015-09-18 18:37:37 -0400

    Eliminacion de Bugs ActividadCiontroller

--- local/app/controllers/admin/SaldosController.php
+++ local/app/controllers/admin/SaldosController.php
@@ -8,35 +8,10 @@ class SaldosController extends \AdminBaseController {
         $this->data['saldosOpen'] ='active open';
         $this->data['pageTitle']  =  'Saldos';
     }
-
-    //    Display a listing of awards
     public function index()
     {
-//        dd("entro aqui");
         $this->data['saldosActive'] =   'active';
         $this->data['saldos']   = Saldo::all();
         return View::make('admin.saldos.index', $this->data);
     }
-
-
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /salary/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-        Salary::destroy($id);
-        $output['success']  =   'deleted';
-        $output['msg']      =   'Salary Deleted successfully';
-        return Response::json($output, 200);
-	}
-
 }
